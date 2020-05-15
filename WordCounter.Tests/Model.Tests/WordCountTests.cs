bea0e422bd15b3f52f95f6c4fe6a6f92c7688b67
brevin <cronkbrevin98@gmail.com>
2020-05-15 16:07:37 -0700

    In this commit I got all the WordCounterTests pasing, had to refactor a little, work on counter getter tests

--- WordCounter.Tests/Model.Tests/WordCountTests.cs
+++ WordCounter.Tests/Model.Tests/WordCountTests.cs
@@ -30,30 +30,34 @@ namespace WordCounter.TestTools
         public void Logic_WillCorrectlyCountMatchedWordsWithCaseInsenitivity_Counter()
         {
             UserInput myInput = new UserInput("bOb", "jeff", "Bob is a really cool guy, BOB once told jeff that Bob is jeff");
-            Assert.AreEqual(4,myInput.CountWords());
+            Assert.AreEqual(3,myInput.CountFirstWord());
+            Assert.AreEqual(2,myInput.CountSecondWord());
         }
         // Test Four
         [TestMethod]
         public void Logic_WillNotCountSimilarWordsInSentence_Counter()
         {
-            UserInput myInput = new UserInput("sea", "Take a seat at the beach near the sea where the sea and the seat are both warm");
-            Assert.AreEqual(2,myInput.CountWords());
+            UserInput myInput = new UserInput("sea", "shirt", "Take a seat at the beach near the sea where the sea and the seat are both warm, just like your shirt, your shirt is pretty like the shire");
+            Assert.AreEqual(2,myInput.CountFirstWord());
+            Assert.AreEqual(2,myInput.CountSecondWord());
         }
 
         // Test Five
         [TestMethod]
         public void Logic_WillCountWordsThatEndWithCommonPunctuation_Counter()
         {
-            UserInput myInput = new UserInput("seat", "Take a seat, Young Skywalker, seat, seat, seat");
-            Assert.AreEqual(4, myInput.CountWords());
+            UserInput myInput = new UserInput("seat","master", "Take a seat, Young Skywalker, we do not grant you the rank of master, so take a seat you're no master");
+            Assert.AreEqual(2,myInput.CountFirstWord());
+            Assert.AreEqual(2,myInput.CountSecondWord());
         }
 
         // Test Six
         [TestMethod]
         public void Logic_WillNotCountWordThatAreIncorrect_Counter()
         {
-            UserInput myInput = new UserInput("Hello", "Hello There, my name is !Hello, its so nice to ,Hello"); 
-            Assert.AreEqual(1, myInput.CountWords());
+            UserInput myInput = new UserInput("Hello", "Yellow", "Hello There, my name is !Hello, its so Yellow like ?yel1ow nice to ,Hello"); 
+            Assert.AreEqual(1,myInput.CountFirstWord());
+            Assert.AreEqual(1,myInput.CountSecondWord());
         }
     }
 }
