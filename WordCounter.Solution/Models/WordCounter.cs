a6f8c03eb79fc93d6832a7f5d617a2a60ce7875e
brevin <cronkbrevin98@gmail.com>
2020-05-15 10:59:00 -0700

    I forgot readme, added it with plain english specs

---
+++ WordCounter.Solution/Models/WordCounter.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collection.Generic;
+
+namespace WordCounter.Models
+{
+    public class Words
+    {
+        private string _word {get;set;}
+        private string _sent {get;set}
+    }
+}
